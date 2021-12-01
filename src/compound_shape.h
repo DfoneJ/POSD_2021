3bba54aac601936c62435497024b5a1f363dcd6b
DfoneJ_School <t110598087@ntut.org.tw>
2021-12-01 20:14:19 +0800

    finish HW4 t5

--- src/compound_shape.h
+++ src/compound_shape.h
@@ -9,24 +9,27 @@ class CompoundShape : public Shape {
 public:
     ~CompoundShape() {}
 
-    double area() const override { return _area; } // 面積
+    double area() const override { 
+        double area = 0;
+        for (Shape* s : _shapes) area += s->area();
+        return area; 
+    } // 面積
 
-    double perimeter() const override { return _perimeter; } // 周長
+    double perimeter() const override { 
+        double perimeter = 0;
+        for (Shape* s : _shapes) perimeter += s->perimeter();
+        return perimeter;
+    } // 周長
 
     std::string info() const override { return std::string("CompoundShape"); } // 資訊
 
     void addShape(Shape* shape) override { // 加入形狀
         _shapes.push_back(shape);
-        _area += shape->area();
-        _perimeter += shape->perimeter();
     }
     
     void deleteShape(Shape* shape) override { // 刪除形狀
         for (Shape* s : _shapes) { 
-            if (s==shape) { //若為其中成員
-                _area -= shape->area(); 
-                _perimeter -= shape->perimeter();
-            }
+            if (s->info() == "CompoundShape") s->deleteShape(shape);
         }
         _shapes.remove(shape); //刪除成員
     }
@@ -39,6 +42,4 @@ public:
 
 private:
     std::list<Shape*> _shapes;
-    double _area = 0.0;
-    double _perimeter = 0.0;
 };
