6ad4ed84c4aab81e19b458a9a2a55dd0eb44999d
labsesdt <labsesdt@gmail.com>
2021-12-29 21:26:31 +0800

    lab2 t2

--- src/visitor/html_visitor.h
+++ src/visitor/html_visitor.h
@@ -1,14 +1,26 @@
 #pragma once
 
 #include "article_visitor.h"
+#include <string>
 
 class HtmlVisitor : public ArticleVisitor {
 public:
-    void visitListItem(ListItem* li) override { }
+    void visitListItem(ListItem* li) override { _result = "<li>"+ li->getText() +"</li>"; } // OK
 
-    void visitText(Text* t) override { }
+    void visitText(Text* t) override { _result = "<span>"+ t->getText() +"</span>"; } // OK
 
-    void visitParagraph(Paragraph* p) override { }
+    void visitParagraph(Paragraph* p) override {
+        std::string P_result="";
+        P_result = P_result + "<h" + str(p->getLevel) + ">" + "</h" + str(p->getLevel) + ">";
+        Iterator* pit = p->createIterator();
+        for(pit->first(); !pit->isDone(); pit->next()) {
+            pit->currentItem()->accept(this);
+            P_result += this->getResult();
+        }
+        _result = P_result;
+    }
 
-    std::string getResult() const override { }
+    std::string getResult() const override { return _result; } // OK
+private:
+    std::string _result="";
 };
